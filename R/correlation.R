dfbe9760fbbb090fbd20ea88234256ced342817f
maous1 <m.bonjean@uclouvain.be>
2022-02-18 15:23:55 +0100

    simulator running
    ajout de la corelation et de la fonction running

---
+++ R/correlation.R
@@ -0,0 +1,31 @@
+#' Title
+#'
+#' @param trainset
+#' @param testset
+#'
+#' @return
+#' @export
+#'
+#' @examples
+correlation <- function(trainset,testset){
+
+  train <- trainset %>% group_by(country_code,time)%>%mutate(somme = sum(new_cases))%>%mutate(pourcentagetrain= new_cases/somme)
+
+  test <- testset%>% group_by(country_code,time,variant)%>%summarise(nb = sum(nb))%>%group_by(country_code,time)%>%mutate(somme = sum(nb))%>%mutate(pourcentagetest= nb/somme)
+  variant <- unique(train$variant)
+  alldatatest<-c()
+  alldatatrain<-c()
+
+  for(var in variant) {
+
+
+    datatrain <- train %>% filter(variant == var) %>%select(country_code,pourcentagetrain,time)
+    dataset <- test %>% filter(variant == var)%>%select(country_code,pourcentagetest,time)
+
+
+    vecteur <-  datatrain %>% left_join(dataset,by = c("country_code", "time"))%>% mutate(pourcentagetest = ifelse(is.na(pourcentagetest),yes = 0,no = pourcentagetest))
+    alldatatrain <- c(alldatatrain,vecteur$pourcentagetrain)
+    alldatatest <- c(alldatatest,vecteur$pourcentagetest)
+  }
+  return(cor(alldatatrain,alldatatest))
+}
--- R/simulator_running.R
+++ R/simulator_running.R
@@ -1,18 +1,20 @@
-#' Simulator: Predict the variant from a training set with a column time and a column variant to a testset with a column time
+#' Title
 #'
-#' @param trainset the data with a column year_week and variant
-#' @param testset the data where the variant will be predicted. Need a year_week column
+#' @param trainset
+#' @param testset
 #' @param time
 #' @param geolocalisation
-#' @param count
 #' @param outcome
+#' @param count
+#' @param factor
 #'
 #' @return
 #' @export
+#'
 #' @examples
-#'
-#'
-simulator <- function(trainset, testset,time ,geolocalisation, outcome, count= NULL){
+simulator_running <- function(trainset, testset,time ,geolocalisation, outcome, count= NULL,factor){
+
+
   set.seed(2)
 
   names(trainset)[names(trainset) %in% geolocalisation]<-"geolocalisation"
@@ -20,16 +22,26 @@ simulator <- function(trainset, testset,time ,geolocalisation, outcome, count= N
   names(testset)[names(testset) %in% geolocalisation]<-"geolocalisation"
   names(testset)[names(testset) %in% time]<-"time"
 
-  unique_geo <- unique(trainset$geolocalisation)
+  #trainset <- trainset %>% mutate(new_cases = as.integer(round(new_cases/factor))) %>% filter(new_cases >0)
+  trainset <- trainset%>%group_by(geolocalisation,time)%>%
+    mutate(somme = sum(new_cases)) %>%
+    mutate(new_cases = as.integer(round(new_cases/somme*factor)))%>%
+    filter(new_cases >0)
 
-  if(length(count)>0){
-    trainset <- expandRows(trainset,count=count,drop=T)
-    testset <- expandRows(testset,count=count,drop=T)
-  }
+  trainset <- expandRows(trainset,count=count,drop=T)
+  testset <- expandRows(testset,count=count,drop=T)
+
+  #trainset <- trainset %>% group_by(geolocalisation,time)%>%slice_sample(n = factor)
+
+  print(length(trainset$time))
+
+
+
 
 
   stat_allgeolocalisation <- data.frame()
   # For each geolocalisation
+  unique_geo <- unique(trainset$geolocalisation)
   for (location in unique_geo) {
 
     # Data train
@@ -41,7 +53,7 @@ simulator <- function(trainset, testset,time ,geolocalisation, outcome, count= N
     V1 <- c("time_jiter")
     train_geolocalisation_V1<- train_geolocalisation[V1]
 
-    class = train_geolocalisation[outcome]
+    classificateur = train_geolocalisation[outcome]
 
 
     # Data test
@@ -53,7 +65,7 @@ simulator <- function(trainset, testset,time ,geolocalisation, outcome, count= N
     # Prediction with KNN model
     pr <- knn(train = data.frame(train_geolocalisation_V1),
               test = data.frame(test_geolocalisation_V1),
-              cl = class[[1]],
+              cl = classificateur[[1]],
               k = 1)
 
     # Create variant variable
@@ -62,7 +74,7 @@ simulator <- function(trainset, testset,time ,geolocalisation, outcome, count= N
     # Concatenate prediction file for all countries
 
     test_geolocalisation <- test_geolocalisation %>%
-      select(-c(time_jiter, time_num)) %>% group_by_all()%>% summarise(nb=n())
+      select(-c(time_jiter, time_num))%>% group_by_all()%>% summarise(nb=n())
     stat_allgeolocalisation <- union_all(stat_allgeolocalisation, test_geolocalisation)
 
   }
@@ -70,4 +82,3 @@ simulator <- function(trainset, testset,time ,geolocalisation, outcome, count= N
   names(stat_allgeolocalisation)[names(stat_allgeolocalisation) %in% "time"]<-time
   return(stat_allgeolocalisation)
 }
-
