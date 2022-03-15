58e39d59ae7aad2d40a08f081d2d3ad6626e70b9
maous1 <m.bonjean@uclouvain.be>
2022-03-15 09:12:35 +0100

    map2

--- R/simulator_combine.R
+++ R/simulator_combine.R
@@ -26,70 +26,7 @@ simulator_combine <- function(trainset, testset,time ,geolocalisation, outcome, 
     mutate(new_cases = as.integer(round(new_cases/somme*factor)))%>%
     filter(new_cases >0)
 
-
-
-
-  unique_geo <- unique(trainset$geolocalisation)
-  date = (trainset %>% group_by(time)%>%
-            summarise(case_week= sum(new_cases))%>%
-            mutate(cum= cumsum(case_week))%>%mutate(index = floor(cum/factor))%>%group_by(index)%>%slice(1))$time
-
-
-  if(length(count)>0){
-    trainset <- expandRows(trainset,count=count,drop=T)
-    testset <- expandRows(testset,count=count,drop=T)
-  }
-
-
-  stat_allgeolocalisation <- data.frame()
-
-
-
-  for (i in 1:(length(date)-1)) {
-    trainset_frag <- trainset %>% filter(as.Date(time)>=as.Date(date[i]) & as.Date(time)<=as.Date(date[i+1]))
-    testset_frag <- testset %>% filter(as.Date(time)>=as.Date(date[i]) & as.Date(time)<=as.Date(date[i+1]))
-
-    # For each geolocalisation
-    for (location in unique_geo) {
-
-      # Data train
-      train_geolocalisation <- trainset_frag %>% filter(geolocalisation == location)
-      train_geolocalisation <- train_geolocalisation %>%
-        mutate(time_num = as.numeric(as.Date(time))) %>%
-        mutate(time_itter = time_num + rnorm(length(time_num),0,3))
-
-      V1 <- c("time_itter")
-      train_geolocalisation_V1<- train_geolocalisation[V1]
-
-      class = train_geolocalisation[outcome]
-
-
-      # Data test
-      test_geolocalisation <- testset_frag %>% filter(geolocalisation == location)
-      test_geolocalisation <- test_geolocalisation %>%
-        mutate(time_num = as.numeric(as.Date(time))) %>%
-        mutate(time_itter = time_num + rnorm(length(time_num),0,3))
-      test_geolocalisation_V1 <- test_geolocalisation[V1]
-      # Prediction with KNN model
-      pr <- knn(train = data.frame(train_geolocalisation_V1),
-                test = data.frame(test_geolocalisation_V1),
-                cl = class[[1]],
-                k = 1)
-
-      # Create variant variable
-      test_geolocalisation[outcome] <- as.character(pr)
-
-      # Concatenate prediction file for all countries
-
-      test_geolocalisation <- test_geolocalisation %>%
-        select(-c(time_itter, time_num)) %>% group_by_all()%>% summarise(nb=n())
-      stat_allgeolocalisation <- union_all(stat_allgeolocalisation, test_geolocalisation)
-
-    }
-  }
-
-  names(stat_allgeolocalisation)[names(stat_allgeolocalisation) %in% "geolocalisation"]<-geolocalisation
-  names(stat_allgeolocalisation)[names(stat_allgeolocalisation) %in% "time"]<-time
-  return(stat_allgeolocalisation)
+alldata <- simulator_fragmentation(trainset=trainset,testset=testset,time=time,geolocalisation=geolocalisation,outcome=outcome,count=count,factor="month")
+return(alldata)
 }
 
--- R/simulator_fragmentation.R
+++ R/simulator_fragmentation.R
@@ -16,72 +16,23 @@
 simulator_fragmentation <- function(trainset, testset,time ,geolocalisation, outcome, count= NULL,factor){
   set.seed(2)
 
-  names(trainset)[names(trainset) %in% geolocalisation]<-"geolocalisation"
   names(trainset)[names(trainset) %in% time]<-"time"
-  names(testset)[names(testset) %in% geolocalisation]<-"geolocalisation"
   names(testset)[names(testset) %in% time]<-"time"
-
-  unique_geo <- unique(trainset$geolocalisation)
-  date = (trainset %>% group_by(time)%>%
-            summarise(case_week= sum(new_cases))%>%
-            mutate(cum= cumsum(case_week))%>%mutate(index = floor(cum/factor))%>%group_by(index)%>%slice(1))$time
-
-
-  if(length(count)>0){
-    trainset <- expandRows(trainset,count=count,drop=T)
-    testset <- expandRows(testset,count=count,drop=T)
+  if(is.numeric(factor)){
+    date <- (trainset %>% group_by(time)%>%
+              summarise(case_week= sum(new_cases))%>%
+              mutate(cum= cumsum(case_week))%>%mutate(index = floor(cum/factor))%>%group_by(index)%>%slice(1))$time
+  }
+  else{
+    date <- unique(unique(trainset$time)%>%mutate(time = as.Date(time,"%y-%m-01")))
   }
 
+  datedepart <- date[-length(date)]
+  dateend <- date[-1]
+  filtretrain <- map2(.x = datedepart,.y = dateend, function(.x,.y) trainset %>% filter(as.Date(time)>=as.Date(.x) & as.Date(time)<as.Date(.y)))
+  filtretestset <- map2(.x = datedepart,.y = dateend, function(.x,.y) testset %>% filter(as.Date(time)>=as.Date(.x) & as.Date(time)<as.Date(.y)))
+  alldata <- map2_df(.x = filtretrain,.y = filtretestset,function(.x,.y) simulator(trainset = .x,testset = .y,time = time,geolocalisation = geolocalisation,outcome = outcome,count=count))
 
-  stat_allgeolocalisation <- data.frame()
-
-
-
-  for (i in 1:(length(date)-1)) {
-    trainset_frag <- trainset %>% filter(as.Date(time)>=as.Date(date[i]) & as.Date(time)<=as.Date(date[i+1]))
-    testset_frag <- testset %>% filter(as.Date(time)>=as.Date(date[i]) & as.Date(time)<=as.Date(date[i+1]))
-
-    # For each geolocalisation
-    for (location in unique_geo) {
-
-      # Data train
-      train_geolocalisation <- trainset_frag %>% filter(geolocalisation == location)
-      train_geolocalisation <- train_geolocalisation %>%
-        mutate(time_num = as.numeric(as.Date(time))) %>%
-        mutate(time_itter = time_num + rnorm(length(time_num),0,3))
-
-      V1 <- c("time_itter")
-      train_geolocalisation_V1<- train_geolocalisation[V1]
-
-      class = train_geolocalisation[outcome]
-
-
-      # Data test
-      test_geolocalisation <- testset_frag %>% filter(geolocalisation == location)
-      test_geolocalisation <- test_geolocalisation %>%
-        mutate(time_num = as.numeric(as.Date(time))) %>%
-        mutate(time_itter = time_num + rnorm(length(time_num),0,3))
-      test_geolocalisation_V1 <- test_geolocalisation[V1]
-      # Prediction with KNN model
-      pr <- knn(train = data.frame(train_geolocalisation_V1),
-                test = data.frame(test_geolocalisation_V1),
-                cl = class[[1]],
-                k = 1)
-
-      # Create variant variable
-      test_geolocalisation[outcome] <- as.character(pr)
-
-      # Concatenate prediction file for all countries
-
-      test_geolocalisation <- test_geolocalisation %>%
-        select(-c(time_itter, time_num)) %>% group_by_all()%>% summarise(nb=n())
-      stat_allgeolocalisation <- union_all(stat_allgeolocalisation, test_geolocalisation)
-
-    }
-  }
-
-  names(stat_allgeolocalisation)[names(stat_allgeolocalisation) %in% "geolocalisation"]<-geolocalisation
-  names(stat_allgeolocalisation)[names(stat_allgeolocalisation) %in% "time"]<-time
-  return(stat_allgeolocalisation)
+return(alldata)
 }
 
