6958d54c57439023e7ad75ac681392001b3a1b0a
MBonjean <67752577+maous1@users.noreply.github.com>
2022-03-18 11:02:10 +0100

    Update simulator_combine.R

--- R/simulator_combine.R
+++ R/simulator_combine.R
@@ -14,27 +14,41 @@
 #'
 #'
 simulator_combine <- function(trainset, testset,time ,geolocalisation, outcome, count= NULL,factor){
-  set.seed(2)
+
   names(trainset)[names(trainset) %in% geolocalisation]<-"geolocalisation"
   names(trainset)[names(trainset) %in% time]<-"time"
   names(testset)[names(testset) %in% geolocalisation]<-"geolocalisation"
   names(testset)[names(testset) %in% time]<-"time"
 
-  geo <- unique(trainset$geolocalisation)
+  ####### split the dataframe in a list of dataframe (one per geolocalisation) and keep common geolocalisation
 
-  geotrain <- map(.x=geo,.f = function(.x) trainset%>%filter(geolocalisation == .x)%>%group_by(time)%>%
-                    mutate(somme = sum(new_cases)) %>%
-                    mutate(new_cases = as.integer(round(new_cases/somme*factor)))%>%
-                    filter(new_cases >0))
-  geotest <- map(.x=geo,.f = function(.x) testset%>%filter(geolocalisation == .x))
+  trainset_list <- trainset %>% split(.$geolocalisation)
+  testset_list <- testset %>% split(.$geolocalisation)
 
+  common_geo <- intersect(names(trainset_list),names(testset_list))
+  trainset_list <- trainset_list[is.element(names(trainset_list),common_geo)]
+  testset_list <- testset_list[is.element(names(testset_list),common_geo)]
 
-  trainset <- trainset%>%group_by(geolocalisation,time)%>%
-    mutate(somme = sum(new_cases)) %>%
-    mutate(new_cases = as.integer(round(new_cases/somme*factor)))%>%
-    filter(new_cases >0)
+  ###### reduce the training dataset
 
-  alldata <- map2_df(.x=geotrain,.y=geotest,.f = function(.x,.y) simulator_fragmentation(trainset=.x,testset=.y,time=time,geolocalisation=geolocalisation,outcome=outcome,count=count,factor="month"))
+  trainset_reduce <- function(trainset_df,factor)
+  {
+    trainset_df <- trainset_df%>%
+      group_by(time)%>%
+      mutate(sum = sum(new_cases)) %>%
+      mutate(new_cases = as.integer(round(factor*new_cases/sum)))%>%
+      filter(new_cases >0) %>%
+      select(-sum)
+    return(trainset_df)
+  }
+
+  trainset_list <- map(.x=trainset_list,.f = trainset_reduce,factor=factor)
+
+  ###### apply the simulator
+
+  alldata <- map2_df(.x=trainset_list,.y=testset_list,.f = function(.x,.y) simulator_fragmentation(trainset=.x,testset=.y,time=time,geolocalisation=geolocalisation,outcome=outcome,count=count,factor="month"))
+
   return(alldata)
+
 }
 
