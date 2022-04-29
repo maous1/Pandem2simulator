44875fdc5483a6eef57fbf89d84ac06077f39d35
MBonjean <67752577+maous1@users.noreply.github.com>
2022-04-29 16:30:18 +0200

    change for multiparametric + change mutation

--- R/mutation.R
+++ R/mutation.R
@@ -1,7 +1,7 @@
 #' mutation
 #'
 #' @param case_variants_genomic
-#' @param numero
+#' @param position
 #'
 #' @return
 #' @export
@@ -12,48 +12,88 @@ mutation <- function(case_variants_genomic,position) {
   if (is.numeric(position)) {
 
 
-  delmissub<- function(numero,substitutions,missing,deletion)
-  {
-    startspike = numero
-    frame1 <- data.frame(substitutions = unlist(strsplit(substitutions,','))) %>%
-      mutate(place = gsub("[^0-9]", "", substitutions))%>%
-      filter(place == (startspike))
-    if (!is_empty(frame1$substitutions)) {
-      return(frame1$substitutions)
-    }
-
-    if (!is.na(deletion)) {
-
-
-      frame2 <-  data.frame(deletions = unlist(strsplit(deletion,','))) %>%
-        group_by(deletions) %>%
-        mutate(start = as.numeric(unlist(strsplit(deletions, '-'))[1]))%>%
-        mutate(end = as.numeric(unlist(strsplit(deletions, '-'))[2]))%>%
-        mutate(end = ifelse(is.na(end),start,end))%>%
-        mutate(inside = between((startspike),start,end))
-
-      if(any(frame2$inside==T)  )
-      {
-        return("deletion")
-      }
-    }
-    if(!is.na(missing))
+    delmissub<- function(position,substitutions,missing,deletion)
     {
-      frame3 <-  data.frame(missing = unlist(strsplit(missing,','))) %>%
-        group_by(missing) %>%
-        mutate(start = as.numeric(unlist(strsplit(missing, '-'))[1]))%>%
-        mutate(end = as.numeric(unlist(strsplit(missing, '-'))[2]))%>%
-        mutate(end = ifelse(is.na(end),start,end))%>%
-        mutate(inside = between((startspike),start,end))
-
-
-      if (any(frame3$inside==T)) {
-        return("missing")
+      frame1 <- data.frame(substitutions = unlist(strsplit(substitutions,','))) %>%
+        mutate(place = gsub("[^0-9]", "", substitutions))%>%
+        filter(place == (position))
+      if (!is_empty(frame1$substitutions)) {
+        return(frame1$substitutions)
       }
+
+      if (!is.na(deletion)) {
+
+
+        frame2 <-  data.frame(deletions = unlist(strsplit(deletion,','))) %>%
+          group_by(deletions) %>%
+          mutate(start = as.numeric(unlist(strsplit(deletions, '-'))[1]))%>%
+          mutate(end = as.numeric(unlist(strsplit(deletions, '-'))[2]))%>%
+          mutate(end = ifelse(is.na(end),start,end))%>%
+          mutate(inside = between((position),start,end))
+
+        if(any(frame2$inside==T)  )
+        {
+          return("deletion")
+        }
+      }
+      if(!is.na(missing))
+      {
+        frame3 <-  data.frame(missing = unlist(strsplit(missing,','))) %>%
+          group_by(missing) %>%
+          mutate(start = as.numeric(unlist(strsplit(missing, '-'))[1]))%>%
+          mutate(end = as.numeric(unlist(strsplit(missing, '-'))[2]))%>%
+          mutate(end = ifelse(is.na(end),start,end))%>%
+          mutate(inside = between((position),start,end))
+
+
+        if (any(frame3$inside==T)) {
+          return("missing")
+        }
+      }
+      return("No mutation")
     }
-    return("No mutation")
+    case_variants_genomic = case_variants_genomic%>% rowwise() %>% mutate(!!as.character(position) := delmissub(position,substitutions,missing,deletions))
+    return(case_variants_genomic)
   }
-  case_variants_genomic = case_variants_genomic%>% rowwise() %>% mutate(!!as.character(numero) := delmissub(numero,substitutions,missing,deletions))
+  if (is.character(position)) {
+    delmissub<- function(position,substitutions,missing,deletion)
+    {
+      if (grepl(pattern = position,x = substitutions)) {
+        return(position)
+      }
+      place = as.numeric(gsub("[^0-9]", "", position))
+      if (!is.na(deletion)) {
+
+
+        frame2 <-  data.frame(deletions = unlist(strsplit(deletion,','))) %>%
+          group_by(deletions) %>%
+          mutate(start = as.numeric(unlist(strsplit(deletions, '-'))[1]))%>%
+          mutate(end = as.numeric(unlist(strsplit(deletions, '-'))[2]))%>%
+          mutate(end = ifelse(is.na(end),start,end))%>%
+          mutate(inside = between((place),start,end))
+
+        if(any(frame2$inside==T)  )
+        {
+          return("deletion")
+        }
+      }
+      if(!is.na(missing))
+      {
+        frame3 <-  data.frame(missing = unlist(strsplit(missing,','))) %>%
+          group_by(missing) %>%
+          mutate(start = as.numeric(unlist(strsplit(missing, '-'))[1]))%>%
+          mutate(end = as.numeric(unlist(strsplit(missing, '-'))[2]))%>%
+          mutate(end = ifelse(is.na(end),start,end))%>%
+          mutate(inside = between((place),start,end))
+
+
+        if (any(frame3$inside==T)) {
+          return("missing")
+        }
+      }
+      return("No mutation")
+    }
+    case_variants_genomic = case_variants_genomic%>% rowwise() %>% mutate(!!position := delmissub(position,substitutions,missing,deletions))
+    return(case_variants_genomic)
   }
-  return(case_variants_genomic)
 }
